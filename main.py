41e9acff9b44a6c1092c6cddf02c2806e80104f0
1-мд-15(ОлегАгеев) <99956143+OlegAgeev@users.noreply.github.com>
2022-03-04 11:28:54 +0300

    g

--- main.py
+++ main.py
@@ -1,1 +1,32 @@
+import telebot  # pyTelegramBotAPI	4.3.1
+from telebot import types
+
+bot = telebot.TeleBot('5112099975:AAF4_2svFifCIPpPMCi0OOm_dqli-gCe1wo')  # Создаем экземпляр бота @Ivanov_Ivan_1MD19_bot
+
+@bot.message_handler (commands=["start"])
+def start(message, res=False):
+    chat_id = message.chat.id
+
+    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
+    btn1 = types.KeyboardButton("Меню")
+    markup.add(btn1)
+
+    bot.send_message(chat_id,
+                     text="Привет, {0.first_name}! Я тестовый бот для курса программирования на языке ПаЙтон".format(
+                         message.from_user), reply_markup=markup)
+
+
+# -----------------------------------------------------------------------
+# Получение сообщений от юзера
+@bot.message_handler(content_types=['text'])
+def get_text_messages(message):
+    chat_id = message.chat.id
+    ms_text = message.text
+    bot.send_message(chat_id, text="Я тебя слышу!!! Ваше сообщение: " + ms_text)
+
+
+
+
+# -----------------------------------------------------------------------
+bot.polling(none_stop=True, interval=0) # Запускаем бота
 
